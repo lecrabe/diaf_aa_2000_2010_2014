ebe17b0277bac4ff2c0d2cf01461423da00ea4ed
remi.dannunzio@gmail.com <remi.dannunzio@gmail.com>
2019-02-08 01:42:41 +0000

    update simulations

--- scripts/bd_rdc_aa_2000_2010.R
+++ scripts/bd_rdc_aa_2000_2010.R
@@ -1,7 +1,12 @@
 ######################################################################################################### 
 ##################### PARTIE I : COMBINER LES CARTES POUR 2000-2010
 ######################################################################################################### 
-
+library(foreign)
+library(plyr)
+library(rgeos)
+library(rgdal)
+library(raster)
+library(ggplot2)
 
 ## Set the working directory
 rootdir       <- "~/diaf_aa_2000_2010_2014/"
@@ -53,16 +58,28 @@ system(sprintf("gdal_calc.py -A %s -B %s --type=Int16 --co=\"COMPRESS=LZW\" --ou
 system(sprintf("rm %s",
                (paste0(datadir,"diaf_0010_16b.tif"))
 ))
+
+##################### IDENTIFIER TOUTES LES COMBINAISONS VALIDES
+classes <- as.numeric(as.vector(outer(1:26, 1:3, paste, sep="")))
+classes <- classes[order(classes)]
+classes
+
+##################### CALCULER LE COMPTAGE DE PIXEL
+system(sprintf("oft-stat %s %s %s",
+               paste0(datadir,"diaf_2000_2010_provinces.tif"),
+               paste0(datadir,"diaf_2000_2010_provinces.tif"),
+               paste0(datadir,"stats_2000_2010.txt")
+))
 ######################################################################################################### 
 ##################### PARTIE II : EXPORTER LE FICHIER DE SUPERFICIES
 ######################################################################################################### 
 
 
 ##################### EXTRAIRE LA RESOLUTION
-pix <- res(raster(paste0(rootdir,"process_2000_2010/diaf_2000_2010_provinces.tif")))[1]
+pix <- res(raster(paste0(datadir,"diaf_2000_2010_provinces.tif")))[1]
 
 ##################### LIRE LA TABLE ET CALCULER LES SUPERFICIES
-df <- read.table(paste0(rootdir,"stats_2000_2010.txt"))[,1:2]
+df <- read.table(paste0(datadir,"stats_2000_2010.txt"))[,1:2]
 names(df) <- c("class","pixel")
 df$area_ha <- df$pixel * pix*pix/10000
 
@@ -74,7 +91,7 @@ df1$change   <- substr(df1$class,nchar(df1$class),nchar(df1$class))
 df1$province <- substr(df1$class,0,nchar(df1$class)-1)
 
 ##################### FUSIONNER AVEC LES NOMS DES PROVINCES
-codes <- read.dbf(paste0(rootdir,"provinces/RDC_Province_26.dbf"))[,c("NOM","ID_SEPAL")]
+codes <- read.dbf(paste0(datadir,"RDC_Province_26.dbf"))[,c("NOM","ID_SEPAL")]
 df2   <- merge(df1,codes,by.y="ID_SEPAL",by.x="province",all.x=T)
 df2   <- arrange(df2,class)[,c("class","NOM","province","change","pixel","area_ha")]
 
@@ -83,11 +100,11 @@ sum(df2$area_ha)
 tapply(df2$area_ha,df2$NOM,sum)
 
 ##################### EXPORTER LE FICHIER
-write.csv(df2,"areas_2000_2010.csv",row.names = F)
+write.csv(df2,paste0(datadir,"areas_2000_2010.csv"),row.names = F)
 
 df3 <- data.frame(cbind(1:3,tapply(df2$area_ha,df2$change,sum)))
 names(df3) <- c("class","area")
-write.csv(df3,"areas_2000_2010_national.csv",row.names = F)
+write.csv(df3,paste0(datadir,"areas_2000_2010_national.csv"),row.names = F)
 
 ######################################################################################################### 
 ##################### PARTIE III : EXPORTER LE FICHIER DE POINTS
